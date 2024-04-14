15eed337b6d2c1e7e2b859790b43faefe229091b
Christophe Maymard <christophe.maymard@hotmail.com>
2024-04-14 23:16:18 +0200

    Replaced render_bg_m5_im2 function definition.
    - Created M5Im2BackgroundLayerRenderer class, from render_bg_m5_im2 function definition, to render background layer in mode 5 with interlace double resolution (IM2) enabled.
    - Created g_bg_layer_renderer_m5_im2 variable to store the instance of the background layer renderer in mode 5 with interlace double resolution (IM2) enabled.
    - Initialized g_bg_layer_renderer_m5_im2 variable in background_layer_rendering_init().
    - Replaced render_bg_m5_im2 function definition by the use of g_bg_layer_renderer_m5_im2 variable.
    - Updated files of add_executable() in CMakeLists.txt.

--- src/core/vdp_render.cpp
+++ src/core/vdp_render.cpp
@@ -76,6 +76,7 @@
 #include "gpgx/ppu/vdp/m5_bg_layer_renderer.h"
 #include "gpgx/ppu/vdp/m5_bg_pattern_cache_updater.h"
 #include "gpgx/ppu/vdp/m5_im2_bg_column_drawer.h"
+#include "gpgx/ppu/vdp/m5_im2_bg_layer_renderer.h"
 #include "gpgx/ppu/vdp/m5_im2_sprite_layer_renderer.h"
 #include "gpgx/ppu/vdp/m5_im2_ste_sprite_layer_renderer.h"
 #include "gpgx/ppu/vdp/m5_satb_parser.h"
@@ -244,6 +245,7 @@ static gpgx::ppu::vdp::M3BackgroundLayerRenderer* g_bg_layer_renderer_m3 = nullp
 static gpgx::ppu::vdp::M3XBackgroundLayerRenderer* g_bg_layer_renderer_m3x = nullptr;
 static gpgx::ppu::vdp::M4BackgroundLayerRenderer* g_bg_layer_renderer_m4 = nullptr;
 static gpgx::ppu::vdp::M5BackgroundLayerRenderer* g_bg_layer_renderer_m5 = nullptr;
+static gpgx::ppu::vdp::M5Im2BackgroundLayerRenderer* g_bg_layer_renderer_m5_im2 = nullptr;
 
 gpgx::ppu::vdp::ISpriteLayerRenderer* g_sprite_layer_renderer = nullptr;
 gpgx::ppu::vdp::TmsSpriteLayerRenderer* g_sprite_layer_renderer_tms = nullptr;
@@ -393,6 +395,41 @@ static void background_layer_rendering_init()
     g_bg_column_drawer_m5_im2 = new gpgx::ppu::vdp::M5Im2BackgroundColumnDrawer(
       atex_table,
       bg_pattern_cache
+    );
+  }
+
+  // Initialize renderer of background layer in mode 5 with interlace 
+  // double resolution (IM2) enabled.
+  if (!g_bg_layer_renderer_m5_im2) {
+    g_bg_layer_renderer_m5_im2 = new gpgx::ppu::vdp::M5Im2BackgroundLayerRenderer(
+      reg,
+      vram,
+      vsram,
+
+      &odd_frame,
+
+      &playfield_shift,
+      &playfield_col_mask,
+      &playfield_row_mask,
+
+      &hscb,
+      &hscroll_mask,
+
+      &ntab,
+      &ntbb,
+      &ntwb,
+
+      linebuf[1],
+      linebuf[0],
+
+      lut[0],
+      lut[2],
+
+      &clip[0],
+      &clip[1],
+
+      &viewport,
+      g_bg_column_drawer_m5_im2
     );
   }
 }
@@ -1369,154 +1406,7 @@ void render_bg_m5_vs(int line)
 
 void render_bg_m5_im2(int line)
 {
-  int column;
-  u32 atex, atbuf, *src, *dst;
-
-  /* Common data */
-  int odd = odd_frame;
-  u32 xscroll      = *(u32 *)&vram[hscb + ((line & hscroll_mask) << 2)];
-  u32 yscroll      = *(u32 *)&vsram[0];
-  u32 pf_col_mask  = playfield_col_mask;
-  u32 pf_row_mask  = playfield_row_mask;
-  u32 pf_shift     = playfield_shift;
-
-  /* Window & Plane A */
-  int a = (reg[18] & 0x1F) << 3;
-  int w = (reg[18] >> 7) & 1;
-
-  /* Plane B width */
-  int start = 0;
-  int end = viewport.w >> 4;
-
-  /* Plane B scroll */
-#ifdef LSB_FIRST
-  u32 shift  = (xscroll >> 16) & 0x0F;
-  u32 index  = pf_col_mask + 1 - ((xscroll >> 20) & pf_col_mask);
-  u32 v_line = (line + (yscroll >> 17)) & pf_row_mask;
-#else
-  u32 shift  = (xscroll & 0x0F);
-  u32 index  = pf_col_mask + 1 - ((xscroll >> 4) & pf_col_mask);
-  u32 v_line = (line + (yscroll >> 1)) & pf_row_mask;
-#endif
-
-  /* Plane B name table */
-  u32 *nt = (u32 *)&vram[ntbb + (((v_line >> 3) << pf_shift) & 0x1FC0)];
-
-  /* Pattern row index */
-  v_line = (((v_line & 7) << 1) | odd) << 3;
-
-  if(shift)
-  {
-    /* Plane B line buffer */
-    dst = (u32 *)&linebuf[0][0x10 + shift];
-
-    atbuf = nt[(index - 1) & pf_col_mask];
-    g_bg_column_drawer_m5_im2->DrawColumn(&dst, atbuf, v_line);
-  }
-  else
-  {
-    /* Plane B line buffer */
-    dst = (u32 *)&linebuf[0][0x20];
-  }
-
-  for(column = 0; column < end; column++, index++)
-  {
-    atbuf = nt[index & pf_col_mask];
-    g_bg_column_drawer_m5_im2->DrawColumn(&dst, atbuf, v_line);
-  }
-
-  if (w == (line >= a))
-  {
-    /* Window takes up entire line */
-    a = 0;
-    w = 1;
-  }
-  else
-  {
-    /* Window and Plane A share the line */
-    a = clip[0].enable;
-    w = clip[1].enable;
-  }
-
-  /* Plane A */
-  if (a)
-  {
-    /* Plane A width */
-    start = clip[0].left;
-    end   = clip[0].right;
-
-    /* Plane A scroll */
-#ifdef LSB_FIRST
-    shift   = (xscroll & 0x0F);
-    index   = pf_col_mask + start + 1 - ((xscroll >> 4) & pf_col_mask);
-    v_line  = (line + (yscroll >> 1)) & pf_row_mask;
-#else
-    shift   = (xscroll >> 16) & 0x0F;
-    index   = pf_col_mask + start + 1 - ((xscroll >> 20) & pf_col_mask);
-    v_line  = (line + (yscroll >> 17)) & pf_row_mask;
-#endif
-
-    /* Plane A name table */
-    nt = (u32 *)&vram[ntab + (((v_line >> 3) << pf_shift) & 0x1FC0)];
-
-    /* Pattern row index */
-    v_line = (((v_line & 7) << 1) | odd) << 3;
-
-    if(shift)
-    {
-      /* Plane A line buffer */
-      dst = (u32 *)&linebuf[1][0x10 + shift + (start << 4)];
-
-      /* Window bug */
-      if (start)
-      {
-        atbuf = nt[index & pf_col_mask];
-      }
-      else
-      {
-        atbuf = nt[(index - 1) & pf_col_mask];
-      }
-
-      g_bg_column_drawer_m5_im2->DrawColumn(&dst, atbuf, v_line);
-    }
-    else
-    {
-      /* Plane A line buffer */
-      dst = (u32 *)&linebuf[1][0x20 + (start << 4)];
-    }
-
-    for(column = start; column < end; column++, index++)
-    {
-      atbuf = nt[index & pf_col_mask];
-      g_bg_column_drawer_m5_im2->DrawColumn(&dst, atbuf, v_line);
-    }
-
-    /* Window width */
-    start = clip[1].left;
-    end   = clip[1].right;
-  }
-
-  /* Window */
-  if (w)
-  {
-    /* Window name table */
-    nt = (u32 *)&vram[ntwb | ((line >> 3) << (6 + (reg[12] & 1)))];
-
-    /* Pattern row index */
-    v_line = ((line & 7) << 1 | odd) << 3;
-
-    /* Plane A line buffer */
-    dst = (u32 *)&linebuf[1][0x20 + (start << 4)];
-
-    for(column = start; column < end; column++)
-    {
-      atbuf = nt[column];
-      g_bg_column_drawer_m5_im2->DrawColumn(&dst, atbuf, v_line);
-    }
-  }
-
-  /* Merge background layers */
-  merge(&linebuf[1][0x20], &linebuf[0][0x20], &linebuf[0][0x20], lut[(reg[12] & 0x08) >> 2], viewport.w);
+  g_bg_layer_renderer_m5_im2->RenderBackground(line);
 }
 
 void render_bg_m5_im2_vs(int line)
